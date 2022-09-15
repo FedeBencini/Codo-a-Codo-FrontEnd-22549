2bb21b39aadb548f7b6801d0c6933b10e1317746
Federico Bencini <89480786+FedeBencini@users.noreply.github.com>
2022-09-15 20:16:39 -0300

    Create clase13

---
+++ clase13/js/operations.js
@@ -0,0 +1,10 @@
+function saludar() {
+  alert("hola mundo desde externo");
+}
+
+/* Operadores */
+let op1 = prompt('Ingrese valor de op1');
+let op2 = prompt('Ingrese valor de op2');
+let res = (+op1) + parseInt(op2);
+//console.log(res);
+alert('resultado = '+res);
